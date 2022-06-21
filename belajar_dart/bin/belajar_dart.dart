1b44daf16d8abc6845a5f1edbb5d93f2e36c2b83
Sulthon <trooulala@gmail.com>
2022-06-22 01:08:31 +0700

    macam-macam comment

--- belajar_dart/bin/belajar_dart.dart
+++ belajar_dart/bin/belajar_dart.dart
@@ -1,5 +1,24 @@
 import 'package:belajar_dart/belajar_dart.dart' as belajar_dart;
 
+/// fungsi [main] akan menampilkan 2 output
+/// output pertama menampilkan teks dan output hasil perkalian
+
 void main(List<String> arguments) {
-  print('Hello world: ${belajar_dart.calculate()}!');
+  // tesing documentation comment []
+  print('6 * 7 = ${belajar_dart.calculate()}');
+
+  // print hello dart
+  print('hello dart');
+
+  // single line comment
+  /* 
+    multi
+    line
+    comment
+  */
+  ///
+  ///multi
+  ///line
+  ///comment
+  ///
 }
